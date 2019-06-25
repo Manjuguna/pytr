969e1aa2f8fac8d2ac37843d534a34d2188fd1bd
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-06-25 12:15:07 +0530

    Update gm3.py

--- gm3.py
+++ gm3.py
@@ -1,5 +1,5 @@
 l=input(" Input the letter of the alphabet() :")
 if l in('a','e','i','o','u'):
-   print("%s is a vowel ." %l)
-elif:
-   print(%s is a constant ."%l)
+   print("%s is a vowel." %l)
+else:
+   print("%s is a constant." %l)
